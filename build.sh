306cbf7c84dbfecefc1567dd120a0764dae68316
Fabin Shen <fabin@fabins19.redmond.corp.microsoft.com>
2019-08-27 16:17:09 -0700

    fix build path

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p linuxbuild
 pushd linuxbuild
-cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
 ninja
 popd
