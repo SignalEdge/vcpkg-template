3d4adde2a77ba640a261cca50075105dcc5e89c9
Fabin Shen <fabin.shen@microsoft.com>
2019-08-27 16:18:29 -0700

    fix build path

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 mkdir winbuild
 pushd winbuild
-cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=..\..\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=..\vcpkg\scripts\buildsystems\vcpkg.cmake ..
 ninja
 popd
